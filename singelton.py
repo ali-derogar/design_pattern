0eaa4d79dc71d0bc1397212dab84be5928f9c756
aliderogar <aliderogar1129@gmail.com>
2023-12-21 20:05:37 +0330

    do logger with singleton

--- singelton.py
+++ singelton.py
@@ -1,6 +1,4 @@
 from typing import Any
-
-
 class Singleton(object):
     
     class SingletonObject():
@@ -37,5 +35,41 @@ s2 = Singleton()
 s2.val = "24"
 print(s)
 print(s2)
+# <__main__.Singleton.SingletonObject object at 0x000002103BE63D60> <-> 12
+# <__main__.Singleton.SingletonObject object at 0x000002103BE63D60> <-> 24
+# <__main__.Singleton.SingletonObject object at 0x000002103BE63D60> <-> 24
 
+from typing import Any
+from source.logs import Loggers
+
+
+class Logger(object):
+        
+    def __init__(self , name) -> None:
+        self.name = name
+    
+    instance = None
+    
+    def __new__(cls , name):
+        
+        if not Logger.instance:
+            Logger.instance = Loggers(f"{__name__}.log")
+            
+            return Logger.instance
+        
+        else:
+            return Logger.instance
+            
+    def __getattr__(self, __name: str) -> Any:
+        return getattr(self.instance , __name)
+        
+    def __setattr__(self, __name: str) -> Any:
+        return setattr(self.instance , __name)
+        
+        
+logger = Logger("logs.log")
+for i in range(15):
+    logger.info(f"state new round {i + 1}")
+    logger.warning(f"{i + 1}"*(i+1))
+    logger.error(f"errorrrrrrrr")
 
